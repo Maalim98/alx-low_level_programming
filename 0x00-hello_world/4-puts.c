beda38931d127c5fcf960f21cdb04dfdd76ad1e3
Maalim98 <ashrafzani585@gmail.com>
2022-12-08 07:21:20 -0800

    tas4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
 
 /**
- *
- *  * main -Printing a string using puts function
- *
- *   * Description: This programme print a string and a new line
- *
- *    * Return: 0
- *
- *    */
+ * main -Printing a string using puts function
+ * Description: This programme print a string and a new line
+ * Return: 0
+ */
 
 int main(void)
 {
-
-		char *s = "\"Programming is like building a multilingual puzzle";
-
-
-
-			puts(s);
-
-
-
-				return (0);
+	char *s = "\"Programming is like building a multilingual puzzle";
+	puts(s);
+	return (0);
 }
-
-
