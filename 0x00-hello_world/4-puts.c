44ef73a364addeb6779a6575f95df1566fd31e98
Maalim98 <ashrafzani585@gmail.com>
2022-12-08 06:22:48 -0800

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main -Printing a string using puts function
+ *
+ *   * Description: This programme print a string and a new line
+ *
+ *    * Return: 0
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+		char *s = "\"Programming is like building a multilingual puzzle";
+
+
+
+			puts(s);
+
+
+
+				return (0);
+
+}
